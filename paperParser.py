fc7f39ff64bcd281ddecf25a77e726dbf3c256d7
Sasha <a.vicente.grab@gmail.com>
2015-09-14 15:59:55 +0200

    Add pands & find DOIs in article

--- paperParser.py
+++ paperParser.py
@@ -1,4 +1,5 @@
 import nltk
+import pandas as pd
 from urllib import urlopen
 from bs4 import BeautifulSoup
 
@@ -12,15 +13,27 @@ soup = BeautifulSoup(html, 'lxml')
 soupArticle = soup.find(class_="article")
 citations = soup.find_all(class_='xref-bibr')
 
+DOIs = list()
+
 # Print all citations to the display
 for c in citations:
     print c.text + ' : ' + c['href']
     
-    cLinks = soup.find_all(id=c['href'][1:])
+    cRef = soup.find_all(id=c['href'][1:])
     
     # Get citation text with DOI link
     # For Cerebral Cortex this is located in the next element
-    e = cLinks[0].next_sibling()
-    print ' '.join(nltk.word_tokenize(e[0].text))
+    cText = cRef[0].next_sibling()
+    print ' '.join(nltk.word_tokenize(cText[0].text))
     
+    # Get HTML links
+    try:
+        doi = cText[0].find('a')['href']
+        DOIs.append(doi)
+    except:
+        print '<***> NO HREF for' + c.text + ' <***>'
+        
     print ''
+
+print 'DOIs:'
+print DOIs
