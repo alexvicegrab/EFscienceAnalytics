f8dd400d7a682de95dae723f921ddd60b4f0b341
Sasha <a.vicente.grab@gmail.com>
2015-09-15 00:20:46 +0200

    Extract text close to relevant citation

--- paperParser.py
+++ paperParser.py
@@ -8,15 +8,22 @@ from bs4 import BeautifulSoup
 # Custom url opener for Google Scholar, which detects bots
 class MyOpener(FancyURLopener):
     version = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'
+
 openurl = MyOpener().open
+
+def url_to_soup(url):
+    html = openurl(url).read()
+    soup = BeautifulSoup(html, 'lxml')
+    #[x.extract() for x in soup(['script','style'])]
+    return soup
 
 # We start by parsing Sasha's first paper in Cerebral Cortex
 
 url = "http://cercor.oxfordjournals.org/content/24/2/281.full"
 
 # Open and import into Beautiful Soup
-html = urlopen(url).read()
-soup = BeautifulSoup(html, 'lxml')
+soup = url_to_soup(url)
+
 soupArticle = soup.find(class_="article")
 
 # Find the article's DOI (in Cerebral Cortex)
@@ -24,18 +31,38 @@ articleDOI = soup.find(class_='slug-doi').text
 
 # Query google scholar for citations
 urlQ = 'http://scholar.google.com/scholar?q=' + articleDOI
-htmlQ = openurl(urlQ).read()
-soupQ = BeautifulSoup(htmlQ, 'lxml')
+
+soupQ = url_to_soup(urlQ)
 
 urlC = "http://scholar.google.com" + soupQ.find(class_='gs_ri').find(class_='gs_fl').a.get('href')
-htmlC = openurl(urlC).read()
-soupC = BeautifulSoup(html, 'lxml')
+
+soupC = url_to_soup(urlC)
+
+citers = list()
 
 for t in soupC.find_all(class_='gs_md_wp'):
     if t.find(class_='gs_ctg2').text == '[HTML]':
-        print t.a.get('href')
+        citers.append( t.a.get('href') )
+
+print citers
+
+# Get first cited paper
+urlL = citers[0]
+
+soupL = url_to_soup(urlL)
 
 
+# Get ID of reference
+for c in soupL.find_all(class_='ref-cit'):
+    if c.get('data-doi') == articleDOI:
+        ref = c.parent.find(class_="rev-xref-ref").get("href")
+
+for c in soupL.find_all(class_="xref-bibr"):
+    if c.get('id') == ref[1:]:
+        print c.parent.text
+        
+
+##############################################
 
 citations = soup.find_all(class_='xref-bibr')
 
