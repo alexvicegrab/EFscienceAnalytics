6963d62a684872c517b80707dfdd695ff11c251f
Sasha <a.vicente.grab@gmail.com>
2015-09-14 21:37:54 +0200

    Find who cited the paper and obtain links to their HTML texts

--- paperParser.py
+++ paperParser.py
@@ -1,7 +1,14 @@
 import nltk
 import pandas as pd
 from urllib import urlopen
+from urllib import FancyURLopener
 from bs4 import BeautifulSoup
+#from selenium import webdriver
+
+# Custom url opener for Google Scholar, which detects bots
+class MyOpener(FancyURLopener):
+    version = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'
+openurl = MyOpener().open
 
 # We start by parsing Sasha's first paper in Cerebral Cortex
 
@@ -11,6 +18,25 @@ url = "http://cercor.oxfordjournals.org/content/24/2/281.full"
 html = urlopen(url).read()
 soup = BeautifulSoup(html, 'lxml')
 soupArticle = soup.find(class_="article")
+
+# Find the article's DOI (in Cerebral Cortex)
+articleDOI = soup.find(class_='slug-doi').text
+
+# Query google scholar for citations
+urlQ = 'http://scholar.google.com/scholar?q=' + articleDOI
+htmlQ = openurl(urlQ).read()
+soupQ = BeautifulSoup(htmlQ, 'lxml')
+
+urlC = "http://scholar.google.com" + soupQ.find(class_='gs_ri').find(class_='gs_fl').a.get('href')
+htmlC = openurl(urlC).read()
+soupC = BeautifulSoup(html, 'lxml')
+
+for t in soupC.find_all(class_='gs_md_wp'):
+    if t.find(class_='gs_ctg2').text == '[HTML]':
+        print t.a.get('href')
+
+
+
 citations = soup.find_all(class_='xref-bibr')
 
 DOIs = list()
